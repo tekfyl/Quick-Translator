7115086a298ab3516ee2cea908acc8622b261c97
Tushar Arora <toshararora@gitlab.com>
2020-12-05 14:14:19 +0530

    Basic translation app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import React, { useState } from "react";
+import Field from "./components/field";
+import Languages from "./components/languages";
+import Translate from "./components/translate";
 
-function App() {
+const App = () => {
+
+  const [language, setLanguage] = useState("ru");
+  const [text, setText] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+    <Field label="Enter English" onChange={setText} value={text} />
+    <Languages language={language} onLanguageChange={setLanguage} />
+    <hr />
+    <Translate text={text} language={language} />
+  </div>
   );
 }
 
